9a86bbc84a889ebf6eb2a6a3d33cabeee4c3915d
Ivan Smirnov <chaekplayyt@gmail.com>
2026-02-18 16:07:05 +0300

    add encode / decode functions

--- src/api/jwt.rs
+++ src/api/jwt.rs
@@ -1,33 +1,84 @@
+use base64::{Engine, engine::general_purpose, prelude::BASE64_URL_SAFE_NO_PAD};
+use hmac::{Hmac, Mac};
 use serde::{Deserialize, Serialize};
+use sha2::{Digest, Sha256};
+
+type HmacSha256 = Hmac<Sha256>;
 
 #[derive(Debug, Serialize, Deserialize)]
-struct Header {
-    alg: &'static str,
-    typ: &'static str
+pub struct Header {
+    alg: String,
+    typ: String
+}
+
+impl Header {
+    pub fn new(alg: String, typ: String) -> Header {
+        Header {
+            alg, typ
+        }
+    }
 }
 
 #[derive(Debug, Serialize, Deserialize)]
-struct Payload {
-    username:&'static str,
-    role: &'static str
+pub struct Payload {
+    username:String,
+    role: String
 }
 
-const SECRET: &str = "mysecret";
+impl Payload {
+    pub fn new(username: String, role: String) -> Payload {
+        Payload { username, role }
+    }
+}
 
-struct JWT {
-    header: Header,
-    payload: Payload,
-    secret: String,
+
+pub const DEFAULT_SECRET: &str = "mysecret";
+
+pub struct JWT {
+    pub header: Header,
+    pub payload: Payload,
+    pub secret: String,
 }
 impl JWT {
-    fn new(header: Header, payload: Payload, secret: String) -> JWT {
+    pub fn new(header: Header, payload: Payload, secret: String) -> JWT {
         JWT {
             header, payload, secret
         }
     }
-    fn gen_signature() -> () {
-        let data_to_hash = "";
+    fn verify_signature(&self, token: &str) -> Result<String, String> {
+        let parts: Vec<&str> = token.split(".").collect();
+        if parts.len() != 3 {
+            return Err("Invalid JWT: expected 3 parts".to_owned())
+        }
+        
+        let (header_base64, payload_base64, signature_base64) = (parts[0], parts[1], parts[2]);
+
+        let data_to_hash = format!("{}.{}", header_base64, payload_base64);
+
+        let signature_bytes = BASE64_URL_SAFE_NO_PAD.decode(signature_base64).unwrap();
+
+        let mut mac = HmacSha256::new_from_slice(&self.secret.as_bytes()).unwrap();
+
+        mac.update(data_to_hash.as_bytes());
+
+        let _ = mac.verify_slice(&signature_bytes);
+
+        Ok("Success".to_string())
+    }   
+    pub fn make_token(&self) -> Result<String, String> {
+        let header_json = serde_json::to_string(&self.header).unwrap();
+        let header_base64 = general_purpose::STANDARD.encode(header_json);
+
+        let payload_json = serde_json::to_string(&self.payload).unwrap();
+        let payload_base64 = general_purpose::STANDARD.encode(payload_json);
+
+        let data_to_hash = format!("{}.{}", header_base64, payload_base64);
+
+        let mut mac = HmacSha256::new_from_slice(self.secret.as_bytes()).unwrap();
+        mac.update(data_to_hash.as_bytes());
+        let signature = mac.finalize().into_bytes();
+
+        let signature_base64 = BASE64_URL_SAFE_NO_PAD.encode(signature);
+        Ok(format!("{}.{}.{}", header_base64, payload_base64, signature_base64))
     }
-    fn verify_signature() {}
-    fn make_token() {}
 }
--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,27 @@ pub mod db;
 pub mod schema;
 
 use crate::api::cors::CORS;
+use crate::api::jwt::Header;
+use crate::api::jwt::Payload;
 use api::comment;
 use api::index::index;
 use api::like;
 use api::post;
 use api::user;
+use api::jwt;
 
 #[macro_use]
 extern crate rocket;
 
 #[launch]
 fn rocket() -> _ {
+    let jwt = jwt::JWT {
+        header: Header::new("HS256".to_owned(), "JWT".to_owned()),
+        payload: Payload::new("lol".to_owned(), "Admin".to_owned()),
+        secret: jwt::DEFAULT_SECRET.to_owned()
+    };
+    let token = jwt.make_token();
+    print!("{}", token.unwrap());
     rocket::build()
         .mount(
             "/api/v1",
