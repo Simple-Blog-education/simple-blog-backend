4e1d9b34f589123cfccda41567bb36a5cde089af
Ivan Smirnov <chaekplayyt@gmail.com>
2026-03-03 14:01:48 +0300

    remove unused imports

--- src/api/cors.rs
+++ src/api/cors.rs
@@ -1,11 +1,18 @@
-use rocket::fairing::{Fairing, Info, Kind};
-use rocket::http::{Header, Method};
-use rocket::{Request, Response};
-use rocket_cors::{AllowedHeaders, AllowedMethods, AllowedOrigins, Cors, CorsOptions};
+use rocket::http::Method;
+use rocket_cors::{AllowedHeaders, AllowedOrigins, Cors, CorsOptions};
 
 pub fn cors_fairing() -> Cors {
     let allowed_origins = AllowedOrigins::All;
-    let allowed_methods = vec![Method::Get, Method::Post, Method::Put, Method::Delete, Method::Options].into_iter().map(From::from).collect();
+    let allowed_methods = vec![
+        Method::Get,
+        Method::Post,
+        Method::Put,
+        Method::Delete,
+        Method::Options,
+    ]
+    .into_iter()
+    .map(From::from)
+    .collect();
     let allowed_headers = AllowedHeaders::All;
     CorsOptions {
         allowed_origins,
@@ -13,6 +20,7 @@ pub fn cors_fairing() -> Cors {
         allowed_headers,
         allow_credentials: true,
         ..Default::default()
-    }.to_cors().expect("Error while creating CORS fairing")
-    
+    }
+    .to_cors()
+    .expect("Error while creating CORS fairing")
 }
