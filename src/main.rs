94bc8f1bd5a9020bf53dd3dd58b457bccbdc8383
Ivan Smirnov <chaekplayyt@gmail.com>
2026-05-20 11:59:22 +0300

    added /auth/me to main function

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,24 @@ pub mod db;
 pub mod routes;
 pub mod schema;
 pub mod services;
-use routes::cors::cors_fairing;
-use services::{auth_service::AuthService, post_service::PostService, user_service::UserService};
 use db::repos::{post_repository::PostRepository, user_repository::UserRepository};
-use routes::{auth_routes, comment_routes, index::index, comment_like_routes, post_routes, user_routes, post_like_routes};
+use routes::cors::cors_fairing;
+use routes::{
+    auth_routes, comment_like_routes, comment_routes, index::index, post_like_routes, post_routes,
+    user_routes,
+};
+use services::{auth_service::AuthService, post_service::PostService, user_service::UserService};
 
-use crate::{db::repos::{comment_like_repository::CommentLikeRepository, comment_repository::CommentRepository, post_like_repository::PostLikeRepository}, services::{comment_like_service::CommentLikeService, comment_service::CommentService, post_like_service::PostLikeService}};
+use crate::{
+    db::repos::{
+        comment_like_repository::CommentLikeRepository, comment_repository::CommentRepository,
+        post_like_repository::PostLikeRepository,
+    },
+    services::{
+        comment_like_service::CommentLikeService, comment_service::CommentService,
+        post_like_service::PostLikeService,
+    },
+};
 
 #[macro_use]
 extern crate rocket;
@@ -34,42 +46,43 @@ fn rocket() -> _ {
     let comment_like_service = CommentLikeService::new(comment_like_repo);
 
     rocket::build()
-    .manage(pool)
-    .manage(user_service)
-    .manage(auth_service)
-    .manage(post_service)
-    .manage(comment_service)
-    .manage(post_like_service)
-    .manage(comment_like_service)
-    .attach(cors_fairing())
-    .mount(
-        "/api/v1",
-        routes![
-            index,
-            auth_routes::sign_in,
-            auth_routes::sign_up,
-            user_routes::get_user,
-            user_routes::user_all,
-            user_routes::delete_user,
-            user_routes::put_user,
-            post_routes::get_post_by_id,
-            post_routes::get_all_posts,
-            post_routes::create_post,
-            post_routes::put_post,
-            post_routes::delete_post,
-            comment_routes::get_comments,
-            comment_routes::get_comments_user,
-            comment_routes::delete_comment,
-            comment_routes::put_comment,
-            comment_routes::post_comment,
-            comment_like_routes::get_comment_likes,
-            comment_like_routes::comment_is_liked_by_user,
-            comment_like_routes::like_comment,
-            comment_like_routes::unlike_comment,
-            post_like_routes::get_post_likes,
-            post_like_routes::post_is_liked_by_user,
-            post_like_routes::like_post,
-            post_like_routes::unlike_post
-        ],
-    )
+        .manage(pool)
+        .manage(user_service)
+        .manage(auth_service)
+        .manage(post_service)
+        .manage(comment_service)
+        .manage(post_like_service)
+        .manage(comment_like_service)
+        .attach(cors_fairing())
+        .mount(
+            "/api/v1",
+            routes![
+                index,
+                auth_routes::sign_in,
+                auth_routes::sign_up,
+                auth_routes::get_current_user,
+                user_routes::get_user,
+                user_routes::user_all,
+                user_routes::delete_user,
+                user_routes::put_user,
+                post_routes::get_post_by_id,
+                post_routes::get_all_posts,
+                post_routes::create_post,
+                post_routes::put_post,
+                post_routes::delete_post,
+                comment_routes::get_comments,
+                comment_routes::get_comments_user,
+                comment_routes::delete_comment,
+                comment_routes::put_comment,
+                comment_routes::post_comment,
+                comment_like_routes::get_comment_likes,
+                comment_like_routes::comment_is_liked_by_user,
+                comment_like_routes::like_comment,
+                comment_like_routes::unlike_comment,
+                post_like_routes::get_post_likes,
+                post_like_routes::post_is_liked_by_user,
+                post_like_routes::like_post,
+                post_like_routes::unlike_post
+            ],
+        )
 }
