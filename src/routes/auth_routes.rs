4f9c25d1f961e8df5fdce121b9559f478c3b5cae
Ivan Smirnov <chaekplayyt@gmail.com>
2026-05-20 11:46:36 +0300

    get current user by token function

--- src/routes/auth_routes.rs
+++ src/routes/auth_routes.rs
@@ -1,27 +1,58 @@
-use rocket::State;
-use rocket::http::Status;
-use crate::db::models::user_models::{LoginCredentials, LoginData, NewUser};
+use crate::db::models::user_models::{LoginCredentials, LoginData, NewUser, User};
+use crate::routes::jwt::Auth;
 use crate::services::auth_service::AuthService;
+use rocket::http::Status;
 use rocket::serde::json::Json;
+use rocket::State;
 
 #[post("/auth/login", format = "json", data = "<data>")]
-pub async fn sign_in(data: Json<LoginCredentials>, service: &State<AuthService>) -> Result<Json<LoginData>, (Status, Json<String>)> {
+pub async fn sign_in(
+    data: Json<LoginCredentials>,
+    service: &State<AuthService>,
+) -> Result<Json<LoginData>, (Status, Json<String>)> {
     match service.sign_in(data.into_inner()).await {
         Ok(token) => Ok(Json(token)),
         Err(e) => {
             eprintln!("Error while sign in: {}", e);
-            Err((Status::InternalServerError, Json("Internal Server Error".to_string())))
+            Err((
+                Status::InternalServerError,
+                Json("Internal Server Error".to_string()),
+            ))
         }
     }
 }
 
 #[post("/auth/signup", format = "json", data = "<data>")]
-pub async fn sign_up(data: Json<NewUser>, service: &State<AuthService>) -> Result<Json<String>, (Status, Json<String>)> {
+pub async fn sign_up(
+    data: Json<NewUser>,
+    service: &State<AuthService>,
+) -> Result<Json<String>, (Status, Json<String>)> {
     match service.sign_up(data.0).await {
         Ok(inserted) => Ok(Json(format!("Inserted {} users", inserted))),
         Err(e) => {
             eprintln!("Error while sign up: {}", e);
-            Err((Status::InternalServerError, Json("Internal Server Error".to_string())))
+            Err((
+                Status::InternalServerError,
+                Json("Internal Server Error".to_string()),
+            ))
+        }
+    }
+}
+
+#[get("/auth/me", format = "json")]
+pub async fn get_current_user(
+    jwt: Auth,
+    service: &State<AuthService>,
+) -> Result<Json<User>, (Status, Json<String>)> {
+    let username = jwt.0;
+    match service.get_current_user(username).await {
+        Ok(user) => Ok(Json(user)),
+        Err(e) => {
+            eprintln!("Error while fetching current user: {}", e);
+            Err((
+                Status::InternalServerError,
+                Json("Internal Server Error".to_string()),
+            ))
         }
     }
 }
--- src/services/auth_service.rs
+++ src/services/auth_service.rs
@@ -1,11 +1,11 @@
+use crate::db::models::user_models::{LoginCredentials, LoginData, NewUser, User};
 use crate::db::models::user_role::UserRole;
-use crate::services::error::ServiceError;
-use crate::routes::jwt::{Claims, JWT, TokenType, get_default_secret};
 use crate::db::repos::user_repository::UserRepository;
-use crate::db::models::user_models::{LoginCredentials, LoginData, NewUser};
+use crate::routes::jwt::{get_default_secret, Claims, TokenType, JWT};
+use crate::services::error::ServiceError;
 
 pub struct AuthService {
-    repo: UserRepository
+    repo: UserRepository,
 }
 
 impl AuthService {
@@ -14,39 +14,64 @@ impl AuthService {
     }
 
     pub async fn sign_up(&self, new_user: NewUser) -> Result<usize, ServiceError> {
-        let inserted = self.repo.create_user(new_user)
-        .await
-        .map_err(ServiceError::from)?
-        .ok_or(ServiceError::Internal)?;
+        let inserted = self
+            .repo
+            .create_user(new_user)
+            .await
+            .map_err(ServiceError::from)?
+            .ok_or(ServiceError::Internal)?;
         Ok(inserted)
     }
 
     pub async fn sign_in(&self, credentials: LoginCredentials) -> Result<LoginData, ServiceError> {
-        let user = self.repo.get_credentials_by_username(credentials.username.clone())
-        .await
-        .map_err(ServiceError::from)?
-        .ok_or(ServiceError::NotFound)?;
+        let user = self
+            .repo
+            .get_credentials_by_username(credentials.username.clone())
+            .await
+            .map_err(ServiceError::from)?
+            .ok_or(ServiceError::NotFound)?;
         if user.password == credentials.password {
-            let id = self.repo.get_id_by_username(credentials.username).await.map_err(ServiceError::from)?.ok_or(ServiceError::NotFound)?;
+            let id = self
+                .repo
+                .get_id_by_username(credentials.username)
+                .await
+                .map_err(ServiceError::from)?
+                .ok_or(ServiceError::NotFound)?;
             let claims = Claims::new(user.username, TokenType::Auth);
-            let token = JWT::make_token(&claims, get_default_secret()).map_err(ServiceError::from)?;
+            let token =
+                JWT::make_token(&claims, get_default_secret()).map_err(ServiceError::from)?;
             let login_data = LoginData {
                 user_id: id.to_string(),
-                token: token
+                token: token,
             };
             Ok(login_data)
+        } else {
+            Err(ServiceError::Validation {
+                reason: "Invalid password".to_string(),
+            })
         }
-        else {
-            Err(ServiceError::Validation { reason: "Invalid password".to_string() })
-        }
-        
     }
     // check for role
-    pub async fn check_user_role(&self, username: String, expected_role: UserRole) -> Result<bool, ServiceError> {
-        let role = self.repo.get_role_by_username(username)
-        .await
-        .map_err(ServiceError::from)?;
+    pub async fn check_user_role(
+        &self,
+        username: String,
+        expected_role: UserRole,
+    ) -> Result<bool, ServiceError> {
+        let role = self
+            .repo
+            .get_role_by_username(username)
+            .await
+            .map_err(ServiceError::from)?;
         Ok(role.value() < expected_role.value())
     }
 
+    pub async fn get_current_user(&self, username: String) -> Result<User, ServiceError> {
+        let user = self
+            .repo
+            .find_by_username(username)
+            .await
+            .map_err(ServiceError::from)?
+            .ok_or(ServiceError::NotFound)?;
+        Ok(user)
+    }
 }
