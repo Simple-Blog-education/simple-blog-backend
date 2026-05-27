d34737c1033b2ca815f87c6df4a73c2f6218805e
Ivan Smirnov <chaekplayyt@gmail.com>
2026-05-27 12:52:52 +0300

    wip: post dto

---
+++ src/db/dto/mod.rs
@@ -0,0 +1,11 @@
+use serde::Serialize;
+
+#[derive(Serialize)]
+pub struct PaginatedResponse<T: Serialize> {
+    pub data: Vec<T>,
+    pub total: i64,
+    pub page: i64,
+    pub per_page: i64,
+}
+
+pub mod post_dto;
--- src/db/mod.rs
+++ src/db/mod.rs
@@ -1,3 +1,4 @@
 pub mod db_connection;
+pub mod dto;
 pub mod models;
 pub mod repos;
